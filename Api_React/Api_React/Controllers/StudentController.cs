524e7220ff6c6da48661ae2b3250a7e9089e343f
Karo <karoll.university@gmail.com>
2021-01-13 17:59:35 -0600

    Creacion de  un CRUD
    Usando frond-end -> React.js
    Usando back-end -> Asp.netCore (API)

---
+++ Api_React/Api_React/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_React.Models.Data;
+using Api_React.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api_React.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        private readonly ILogger<StudentController> _logger;
+        private readonly IConfiguration _configuration;
+
+        StudentData studentData;
+
+        public StudentController(ILogger<StudentController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public List<Student> Get()
+        {
+            studentData = new StudentData(_configuration);
+            return studentData.GetStudent().ToList<Student>();
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            studentData = new StudentData(_configuration);
+            return studentData.GetStudentById(id);
+           
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            studentData = new StudentData(_configuration);
+            int resultToReturn = studentData.Insert(student);
+            //llamada al modelo para insertar el estudiante
+           return  Ok(resultToReturn);
+                   }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{studentId}")]
+        public IActionResult Put(int studentId, [FromBody] Student student)
+        {
+            studentData = new StudentData(_configuration);
+            int resultToReturn = studentData.Update(studentId, student);
+            return  Ok(resultToReturn);
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{studentId}")]
+        public IActionResult Delete(int studentId)
+        {
+            studentData = new StudentData(_configuration);
+            int resultToReturn = studentData.Delete(studentId);
+            return Ok(resultToReturn);
+        }
+    }
+}
---
+++ Api_React/Api_React/Models/Data/StudentData.cs
@@ -0,0 +1,130 @@
+﻿using Api_React.Models.Domain;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_React.Models.Data
+{
+    public class StudentData
+    {
+        private readonly IConfiguration _configuration;
+        string connectionString;
+
+        public StudentData(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public StudentData()
+        { }
+
+        public List<Student> GetStudent()
+        {
+            List<Student> listStudent = new List<Student>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("GetStudent", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Student student = new Student()
+                    {
+                        StudentId = Convert.ToInt32(reader["StudenId"]),
+                        Name = Convert.ToString(reader["Name"]),
+                        Email = Convert.ToString(reader["Email"]),
+                        Password = Convert.ToString(reader["Password"]),
+                    };
+                    listStudent.Add(student);
+                }
+                connection.Close();
+                return listStudent;
+            }
+        }
+
+        public Student GetStudentById(int studentId) {
+            Student listStudent = new Student();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("GetStudentById", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StudentId", studentId);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    listStudent = new Student(reader.GetInt32(0),reader.GetString(1),reader.GetString(2),reader.GetString(3));
+                   
+                }
+                connection.Close();
+                return listStudent;
+            }
+
+            }
+
+        public int Insert(Student student)
+        {
+            int resultToReturn;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("InsertStudent", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Name", student.Name);
+                command.Parameters.AddWithValue("@Email", student.Email);
+                command.Parameters.AddWithValue("@Password", student.Password);
+                //  com.ExecuteNonQuery();
+                resultToReturn = command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            return resultToReturn;
+        }
+
+        public int Update(int id,Student student)
+        {
+            int resultToReturn;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UpdateStudent", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StudentId", id);
+                command.Parameters.AddWithValue("@Name", student.Name);
+                command.Parameters.AddWithValue("@Email", student.Email);
+                command.Parameters.AddWithValue("@Password", student.Password);
+                resultToReturn = command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            return resultToReturn;
+        }
+        public int Delete(int studentId)
+        {
+            int resultToReturn;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("DeleteStudent", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StudentId", studentId);
+               
+                resultToReturn = command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            return resultToReturn;
+        }
+
+    }
+}
